47a463da2987a046076c4a068949d3aa356f1e40
ijgreenidge <igreenidge2018@bklawtech.com>
2017-04-29 20:26:40 +0000

    finish

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-Welcome to "Unnamed_Poject" the futuristic themed Personal Assistant:
+Welcome to your Destiny, your very own futuristic themed Personal Assistant:
 Made to make your personal experiences easier
-
-- Formatted in a convenient dashboard.
-
-- Able to predict the future: You, Future Champions of Sports Leagues (NBA, NFL etc.).
-
-- Youtube and Twitch support for livestreams and keeping up to date with the latest ESPORTS.
 
 - Simple design user friendly.
 
 - We built this using HTML, CSS, Bootstrap and Javascript/Jquery.
 
--
+- Tells you the weather 
+
+- Helps you with your mood. 
+
+Project Manager: Imani G.
+
+Designer: Raishan B.
+
+Developers: Daniel Z. and Henry C.
+
+We envisioned a way to make connectivity better as time progesses. We thought that a person assistant would be a perfect way to capture this.
+
+We had challenges such as time management and collecting and combining all of out ideas.
+
+We plan to expand on the possibilities that Destiny can be able to do, such as be adaptable to you or be able to
 
 
 
---
+++ script.js
@@ -0,0 +1,86 @@
+/*global $*/
+var user = {};
+var jokes = ["What do you call a broken pencil? Pointless!", "Knock Knock. 'Why did you knock?'. 'Theodore'. 'What?'. 'Theodore wasnt open so I knocked'."];
+var questionState = "name";
+
+
+$("#userInput").on('change', function(e) {
+    var input = e.target.value;
+
+    switch (questionState) {
+        case "name":
+            user.name = input;
+            $("#question").addClass("animated fadeInRightBig");
+            changeText("How are you feeling today " + user.name +  ", good or bad?");
+            questionState = "mood";
+            break;
+        case "mood":
+            if (input === "good") {
+                user.mood = "good";
+                $("#question").addClass("animated fadeInRightBig");
+                changeText("Thats great. Keep at it " + user.name);
+                $("#output").html("<img class='img-fluid' id='goodImage' src='http://pix.iemoji.com/images/emoji/apple/ios-9/256/thumbs-up-sign.png' />");
+            }
+            else if (input === "bad") {
+                user.mood = "bad";
+                changeText("Thats too bad. Here is a joke " + user.name);
+                var random = Math.floor(Math.random() * jokes.length);
+                $("#output").html(jokes[random]);
+            }questionState = "weather";
+            break;
+        case "weather":
+            getWeather();
+            questionState = "done";
+            break;
+    }
+    if(input == "thanks"){
+        $("#question").html("No problem " + user.name + " :D");
+    }
+    else if(input == "no problem"){
+        $("#question").html("Anytime " + user.name + " :D");
+    }
+    else if(input == "whats my name"){
+        $("#question").html("Your name is " + user.name);
+    }    
+    else if(input.includes("weather")){
+        getWeather();
+    }   
+});
+
+function changeText(prompt) {
+    $("#question").html(prompt);
+    $("#userInput").val("");
+}
+
+
+var demoUser = {
+    "location": "New York City",
+    "Name": "Henry",
+    "age": "18",
+    "summer_outfit": {
+        "description": "summer causal",
+        "image": "https://s-media-cache-ak0.pinimg.com/736x/e7/b5/cc/e7b5cc7e115f784dc20ad23fcb5985be.jpg"
+    }
+};
+
+function printWeather(response) {
+    var temp = response.main.temp_max;
+    var image = demoUser.summer_outfit.image;
+    var currentlocation = response.name;
+    var current = response.weather[0].main;
+    var imageElement = $('<img>', {
+        src: image,
+        id: "outfit"
+    });
+    $('#output').empty();
+    $('#output').append("Todays weather for " + currentlocation + " is " + current + " with a high of " + temp + ". I recommend this outfit for today.", imageElement);
+}
+
+function getWeather() {
+    var url = 'http://api.openweathermap.org/data/2.5/weather' +
+        '?appid=c2f2d170f6f6fc336058e9851edb828c' +
+        '&q=' + demoUser.location +
+        '&units=imperial';
+
+    $.get(url, printWeather);
+}
